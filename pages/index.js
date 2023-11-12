4bc982fe63fbdd277c3e0cc88b52fbeefd888ece
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2023-11-12 17:10:45 +0100

    Just removed fixed a bug

--- pages/index.js
+++ pages/index.js
@@ -130,33 +130,35 @@ export default function Home({tweets}) {
 }
 
 
-export async function getServerSideProps(context){
+
+
+// export async function getServerSideProps(context){
   
-  const client = new Client(); 
+//   const client = new Client(); 
    
    
-   client
-   .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT).setProject(process.env.NEXT_PUBLIC_PROJECT);
+//    client
+//    .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT).setProject(process.env.NEXT_PUBLIC_PROJECT);
 
    
-   const databases = new Databases(client); 
+//    const databases = new Databases(client); 
    
-   const tweets = await databases.listDocuments(
+//    const tweets = await databases.listDocuments(
    
-   process.env.NEXT_PUBLIC_DATABASE, 
-   process.env.NEXT_PUBLIC_TWEETS_COLLECTION
+//    process.env.NEXT_PUBLIC_DATABASE, 
+//    process.env.NEXT_PUBLIC_TWEETS_COLLECTION
    
 
-   );
+//    );
    
    
-   return{
+//    return{
     
-    props: {tweets}, 
+//     props: {tweets}, 
     
     
     
-   }
+//    }
   
 
   
